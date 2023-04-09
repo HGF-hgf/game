202746a0a09849c1eb670ad4f98a1a4afa2fdec5
HGF-hgf <ngvh1110@gmail.com>
2023-04-09 17:01:39 +0700

    readme fixed animation files

--- SeqAnimation.cpp
+++ SeqAnimation.cpp
@@ -1,55 +1,55 @@
-#include "SeqAnimation.h"
-#include "tinyxml.h"
-#include "iostream"
-#include "Texture.h"
-
-SeqAnimation::SeqAnimation(bool repeat) :Animation(repeat) {}
-
-void SeqAnimation::DrawFrame(float x, float y, float xScale, float yScale, SDL_RendererFlip flip) {
-    Texture::Getinstance()->Draw(m_CurrentSeq.TextureIDs[m_CurrentFrame], x, y, m_CurrentSeq.Width, m_CurrentSeq.Height, xScale, yScale, flip);
-}
-
-void SeqAnimation::Update(float dt) {
-
-    if (m_Repeat || !m_IsEnded) {
-        m_IsEnded = false;
-        m_CurrentFrame = (SDL_GetTicks() / m_CurrentSeq.Speed) % m_CurrentSeq.FrameCount;
-    }
-
-    if (!m_Repeat && m_CurrentFrame == (m_CurrentSeq.FrameCount - 1)) {
-        m_IsEnded = true;
-        m_CurrentFrame = (m_CurrentSeq.FrameCount - 1);
-    }
-}
-
-void SeqAnimation::SetCurrentSeq(std::string seqID) {
-    if (m_SeqMap.count(seqID) > 0)
-        m_CurrentSeq = m_SeqMap[seqID];
-    else
-        std::cout << "The given Sequence animation is not matching: " << seqID << std::endl;
-}
-
-void SeqAnimation::Parse(std::string source) {
-
-    TiXmlDocument xml;
-    xml.LoadFile(source);
-    if (xml.Error())
-        std::cout << "Failed to load animation file: " << source << std::endl;
-
-    TiXmlElement* root = xml.RootElement();
-    for (TiXmlElement* e = root->FirstChildElement(); e != nullptr; e = e->NextSiblingElement()) {
-        if (e->Value() == std::string("sequence")) {
-            Sequence seq;
-            std::string seqID = e->Attribute("id");
-            e->Attribute("speed", &seq.Speed);
-            e->Attribute("width", &seq.Width);
-            e->Attribute("height", &seq.Height);
-            e->Attribute("frameCount", &seq.FrameCount);
-
-            for (TiXmlElement* frame = e->FirstChildElement(); frame != nullptr; frame = frame->NextSiblingElement())
-                seq.TextureIDs.push_back(frame->Attribute("textureID"));
-
-            m_SeqMap[seqID] = seq;
-        }
-    }
-}
+//#include "SeqAnimation.h"
+//#include "tinyxml.h"
+//#include "iostream"
+//#include "Texture.h"
+//
+//SeqAnimation::SeqAnimation(bool repeat) :Animation(repeat) {}
+//
+//void SeqAnimation::DrawFrame(float x, float y, float xScale, float yScale, SDL_RendererFlip flip) {
+//    Texture::Getinstance()->Draw(m_CurrentSeq.TextureIDs[m_CurrentFrame], x, y, m_CurrentSeq.Width, m_CurrentSeq.Height, xScale, yScale, flip);
+//}
+//
+//void SeqAnimation::Update(float dt) {
+//
+//    if (m_Repeat || !m_IsEnded) {
+//        m_IsEnded = false;
+//        m_CurrentFrame = (SDL_GetTicks() / m_CurrentSeq.Speed) % m_CurrentSeq.FrameCount;
+//    }
+//
+//    if (!m_Repeat && m_CurrentFrame == (m_CurrentSeq.FrameCount - 1)) {
+//        m_IsEnded = true;
+//        m_CurrentFrame = (m_CurrentSeq.FrameCount - 1);
+//    }
+//}
+//
+//void SeqAnimation::SetCurrentSeq(std::string seqID) {
+//    if (m_SeqMap.count(seqID) > 0)
+//        m_CurrentSeq = m_SeqMap[seqID];
+//    else
+//        std::cout << "The given Sequence animation is not matching: " << seqID << std::endl;
+//}
+//
+//void SeqAnimation::Parse(std::string source) {
+//
+//    TiXmlDocument xml;
+//    xml.LoadFile(source);
+//    if (xml.Error())
+//        std::cout << "Failed to load animation file: " << source << std::endl;
+//
+//    TiXmlElement* root = xml.RootElement();
+//    for (TiXmlElement* e = root->FirstChildElement(); e != nullptr; e = e->NextSiblingElement()) {
+//        if (e->Value() == std::string("sequence")) {
+//            Sequence seq;
+//            std::string seqID = e->Attribute("id");
+//            e->Attribute("speed", &seq.Speed);
+//            e->Attribute("width", &seq.Width);
+//            e->Attribute("height", &seq.Height);
+//            e->Attribute("frameCount", &seq.FrameCount);
+//
+//            for (TiXmlElement* frame = e->FirstChildElement(); frame != nullptr; frame = frame->NextSiblingElement())
+//                seq.TextureIDs.push_back(frame->Attribute("textureID"));
+//
+//            m_SeqMap[seqID] = seq;
+//        }
+//    }
+//}
--- SeqAnimation.h
+++ SeqAnimation.h
@@ -1,32 +1,32 @@
-#pragma once
-#include<map>
-#include<string>
-#include<vector>
-#include<SDL.h>
-#include"Animation.h"
-
-using namespace std;
-
-struct Sequence {
-	int Speed;
-	int FrameCount;
-	int Width, Height;
-	std::vector<std::string> TextureIDs; 
-};
-
-class SeqAnimation: public Animation
-{
-public:
-	SeqAnimation(bool repeat = true);
-
-	virtual void Update(float dt);
-	void Parse(string source);
-	void SetCurrentSeq(string seqID);
-	void SetRepeat(bool repeat) { m_Repeat = repeat; }
-	void DrawFrame(float x, float y, float xScale = 1, float yScale = 1, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-private:
-	Sequence m_CurrentSeq; // player state
-	std::map<std::string, Sequence> m_SeqMap;
-};
-
+//#pragma once
+//#include<map>
+//#include<string>
+//#include<vector>
+//#include<SDL.h>
+//#include"Animation.h"
+//
+//using namespace std;
+//
+//struct Sequence {
+//	int Speed;
+//	int FrameCount;
+//	int Width, Height;
+//	std::vector<std::string> TextureIDs; 
+//};
+//
+//class SeqAnimation: public Animation
+//{
+//public:
+//	SeqAnimation(bool repeat = true);
+//
+//	virtual void Update(float dt);
+//	void Parse(string source);
+//	void SetCurrentSeq(string seqID);
+//	void SetRepeat(bool repeat) { m_Repeat = repeat; }
+//	void DrawFrame(float x, float y, float xScale = 1, float yScale = 1, SDL_RendererFlip flip = SDL_FLIP_NONE);
+//
+//private:
+//	Sequence m_CurrentSeq; // player state
+//	std::map<std::string, Sequence> m_SeqMap;
+//};
+//
